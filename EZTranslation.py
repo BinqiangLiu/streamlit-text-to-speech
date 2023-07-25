4e6f9696f97349c76b146e66218d0062d6f73cb4
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-25 18:51:15 +0800

    Update EZTranslation.py

--- EZTranslation.py
+++ EZTranslation.py
@@ -130,9 +130,10 @@ display_output_text = st.checkbox("听语音（并显示翻译结果）")
 if text is None:
     st.write("请在上方输入框中输入需要翻译的内容")
 elif display_output_text:
+    output_text = text_to_speech(input_language, output_language, text)
     audio_file = open("translationresult.mp3", "rb")
     audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format="audio/mp3")
+    st.audio("translationresult.mp3", format="audio/mp3")
     st.write(f" {output_text}")
 
  
