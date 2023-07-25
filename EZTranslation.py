c931dabfe110dff466cff4e8aeed0ed8a40285ce
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-25 19:05:25 +0800

    Update EZTranslation.py

--- EZTranslation.py
+++ EZTranslation.py
@@ -135,9 +135,8 @@ elif display_output_text:
     output_text = text_to_speech(input_language, output_language, text)
     audio_file = open("translationresult.mp3", "rb")
     audio_bytes = audio_file.read()
-    st.audio("translationresult.mp3", format="audio/mp3")
+    st.audio("translationresult.mp3")
     st.write(f" {output_text}")
-
  
 #    在手机端，下面这行代码会导致错误（手机上无法播放）
 #    st.audio(audio_bytes, format="audio/mp3", start_time=0)
