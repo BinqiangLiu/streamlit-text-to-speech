840e81c462e0499c2c4e7b3252c45f09dcda47f7
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-25 16:45:56 +0800

    Update NewApp4Render.py

--- NewApp4Render.py
+++ NewApp4Render.py
@@ -71,7 +71,7 @@ elif out_lang == "Korea":
 
 st.write("---")
 
-def text_to_speech(input_language, output_language, text, tld):
+def text_to_speech(input_language, output_language, text):
     translation = translator.translate(text, src=input_language, dest=output_language)
     trans_text = translation.text
     tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
