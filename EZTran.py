6f07ecf620c543c62aff8614f9aad465bd64d402
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-26 13:56:31 +0800

    Update EZTran.py

--- EZTran.py
+++ EZTran.py
@@ -24,8 +24,6 @@ st.title("易翻译 | Easy Translation")
 
 st.write("---")
 
-translator = Translator()
-
 in_lang = st.selectbox(
     "请选择待翻译文本的语言",
     ("Chinese", "English", "German", "French", "Japanese", "Korean"),
@@ -67,59 +65,55 @@ elif out_lang == "Korean":
 
 st.write("---")
 
-# Pre-filled text for the text_input widget
-#pre_filled_text = "Hi"
-
-# Create the text_input widget with pre-filled text
-#text = st.text_input("输入需要翻译的内容", value=pre_filled_text)
-#text = st.text_input(label="输入需要翻译的内容", value="易翻译EasyTransation", placeholder='在此输入Enter here')
-text = st.text_input(label="输入需要翻译的内容", value="", placeholder='在此输入Enter here')
-
 tips_text = "请在上方输入框中输入需要翻译的内容"
+
+#text = st.text_input(label="输入需要翻译的内容", value="易翻译EasyTransation", placeholder='在此输入Enter here')
+
+translator = Translator()
+placeholder = st.empty()
+
+text = placeholder.text_input(label="输入需要翻译的内容（请务必先输入要翻译的内容再查看翻译或播放语音）", value="", placeholder='在此输入Enter here', key=1)
+
+display_output_text = st.checkbox("语音播放翻译结果")
+
+click_clear = st.button('清空输入框', key=3)
 
 def text_to_speech(input_language, output_language, text):
     try:
         translator = Translator()
         translation = translator.translate(text, src=input_language, dest=output_language)
         trans_text = translation.text
+        tts = gTTS(trans_text, lang=output_language, slow=False)
+        tts.save("translationresult.mp3")
         return trans_text
     except Exception as e:
         # Handle the error, e.g., print an error message or return a default text
         print(f"Translation error: {e}")
-        return "请先输入需要翻译的内容Enter contents to translate first"
+        return "要翻译内容为空"
+        st.stop()
 
-if text is not None:
-    st.write("翻译结果")
-    output_text = text_to_speech(input_language, output_language, text)
-    st.write(f" {output_text}")
-else:
-    st.write("请在上方输入框中输入需要翻译的内容")
+if text is None:
     st.stop()
+else: 
+    if text is not None:
+        st.write("翻译结果")
+        output_text = text_to_speech(input_language, output_language, text)
+        st.write(f" {output_text}")
+    else:
+        st.write("请在上方输入框中输入需要翻译的内容")
+        st.stop()
+    if click_clear:
+        text = placeholder.text_input(label="输入需要翻译的内容（请务必先输入要翻译的内容再查看翻译或播放语音）", value="", placeholder='在此输入Enter here', key=2)
+        st.stop()
 
-display_output_text = st.checkbox("语音播放翻译结果")
-if display_output_text:
-    output_text = text_to_speech(input_language, output_language, text)
-    audio_file = open("translationresult.mp3", "rb")
-    audio_bytes = audio_file.read()
-    st.audio("translationresult.mp3")
-#    st.write(f" {output_text}")
-#if text is None:
-#    st.write("请在上方输入框中输入需要翻译的内容")
-#    st.stop()
- 
-#    在手机端，下面这行代码会导致错误（手机上无法播放）
-#    st.audio(audio_bytes, format="audio/mp3", start_time=0)
-
-#os.remove(f"translationaudio.mp3")
-
-#def remove_files(n):
-#    mp3_files = glob.glob("temp/*mp3")
-#    if len(mp3_files) != 0:
-#        now = time.time()
-#        n_days = n * 86400
-#        for f in mp3_files:
-#            if os.stat(f).st_mtime < now - n_days:
-#                os.remove(f)
-#                print("Deleted ", f)
-
-#remove_files(7)
+    if display_output_text:
+        try:
+            output_text = text_to_speech(input_language, output_language, text)
+            audio_file = open("translationresult.mp3", "rb")
+            audio_bytes = audio_file.read()
+            st.audio("translationresult.mp3")
+        except Exception as e:
+            # Handle the error, e.g., print an error message or return a default text
+            print(f"Translation error: {e}")
+            st.write("要翻译内容为空")
+            st.stop()
