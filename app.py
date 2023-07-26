95ecca5b7987dbbeb380e3be836b0acade1c9dcf
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-26 10:33:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.title("文字/文本转语音App-Streamlit")
 #text = st.text_input("【输入的文本中不可以包括符号【/等文件命名不允许的字符】，因为涉及【f+temp+/+my_file_name.mp3文件命名】")
 translator = Translator()
 
-text = st.text_input("输入需要翻译的内容（注意：输入的文本中不可以包括符号/）")
+text = st.text_input("输入需要翻译的内容（注意：输入的文本中不可以包括符号/）", placeholder="INPUT HERE")
 in_lang = st.selectbox(
     "请选择输入语言",
     ("English", "Hindi", "Bengali", "korean", "Chinese Simplified", "Chinese Traditional", "Japanese"),
